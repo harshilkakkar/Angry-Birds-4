05b33ab4b1809022ff3dd9cb4d5f1629b529bb81
Harshil Kakkar <kakkar.harshil@gmail.com>
2021-01-12 09:12:49 +1100

    Today we worked on the sling shot

--- Slingshot.js
+++ Slingshot.js
@@ -6,22 +6,49 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
-        this.pointB = pointB
+         this.sling1 = loadImage("sprites/sling1.png");
+         this.sling2 = loadImage("sprites/sling2.png");
+         this.sling3 = loadImage("sprites/sling3.png");
+        
+         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
 
+    
     fly(){
         this.sling.bodyA = null;
     }
 
     display(){
+        image(this.sling1, 175, 20);
+        image(this.sling2, 150, 20);
+        
+        
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            push();
+           
+            if(pointA.x < 190) {
+            stroke(48,22,8);
+            strokeWeight(10);
+            line(pointA.x - 25 , pointA.y, pointB.x - 14, pointB.y);
+            line(pointA.x - 25 , pointA.y, pointB.x + 14, pointB.y);
+            image(this.sling3, pointA.x - 35, pointA.y, 15, 30);
+            }
+           else{
+            stroke(48,22,8);
+            strokeWeight(5);
+            line(pointA.x + 25 , pointA.y, pointB.x + 14, pointB.y);
+            line(pointA.x + 25 , pointA.y, pointB.x - 14, pointB.y);
+            image(this.sling3, pointA.x + 20, pointA.y, 15, 30);
+
+           }
+            pop();
+          
+        
         }
     }
-    
+      
 }
